d865a9d367e1b11933d70726f9696773dc638382
firdausgith <muhammadfirdausalfrz@gmail.com>
2025-02-19 12:41:42 +0700

    Add files via upload

---
+++ team/Firdaus.py
@@ -0,0 +1,15 @@
+def perkenalan(nama, nim, peran): 
+    print("Halo! Perkenalkan, nama saya", nama)
+    print("NIM :", nim)
+    print("Peran saya yaitu:")
+    print(peran)
+
+if __name__ == "__main__":
+    nama = "Muhammad Firdaus Al-Farizi"
+    nim = "2213010411"
+    peran = (
+        "- Implementasi fitur tambah, edit, hapus, dan daftar perjalanan\n"
+        "- Implementasi model perjalanan untuk mengelola data perjalanan\n"
+        "- Implementasi model pengguna untuk autentikasi pada models.py"
+    )
+    perkenalan(nama, nim, peran)
